c7fe0ca68afc8bae5280a15de2669787705693b4
redia047 <renatafiori@hotmail.it>
2025-04-03 01:40:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask import
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')def home():return"L'app funziona!"
+@app.route('/')def home():return "L'app funziona!"
 
-if__name__=='__main__':
+if __name__=='__main__':
 
 app.run(host='0.0.0.0',port=10000)
 
